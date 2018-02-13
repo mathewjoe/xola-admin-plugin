00a4894d52636bf28095d46f13f2d47b179d9b21
Mathew Joe Thomas <disrupticons@gmail.com>
2018-02-14 02:58:55 +0530

    Allow specifying email & password instead of api key when creating new environments
    Additional changes:
    - When adding new environment, verify the credentials for the specified domain
    - Show error message when verification fails

--- src/configuration_page/index.js
+++ src/configuration_page/index.js
@@ -1,4 +1,5 @@
 /*global chrome*/
+import $ from 'jquery';
 import 'spectre.css/dist/spectre-icons.min.css';
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
@@ -40,43 +41,90 @@ class EnvironmentConfigList extends Component {
    }
 }
 
-const PROTOCOL = {HTTP: 'http://', HTTPS: 'https://'};
+const PROTOCOL = {HTTPS: 'https://', HTTP: 'http://'};
 
 class AddEnvironmentConfig extends Component {
    constructor(props) {
       super(props);
-      this.state = {
+      this.state = this.getDefaultState();
+   }
+
+   getDefaultState() {
+      return {
          env: {
             name: '',
             protocol: PROTOCOL.HTTPS,
-            domain: '', apiKey: '',
+            domain: '',
+            apiKey: '',
+            email: '',
+            password: '',
             get url() {
                return this.protocol + this.domain;
             }
          },
-         loading: false
+         useApiKey: false,
+         loading: false,
+         error: false
       };
    }
 
    render() {
+      const emailIdFields = (
+          <div className="form-group">
+             <div className="form-group">
+                <input className={"form-input" + (this.state.error ? " is-error" : "")} type="text"
+                       placeholder="Email"
+                       name="email"
+                       value={this.state.env.email} onChange={this.handleInputChange}/>
+             </div>
+             <div className="form-group">
+                <input className={"form-input" + (this.state.error ? " is-error" : "")} type="password"
+                       placeholder="Password"
+                       name="password"
+                       value={this.state.env.password} onChange={this.handleInputChange}/>
+                {this.state.error ?
+                    <p className="form-input-hint">Unable to verify credentials. Please check your domain and
+                       credentials.</p> : ""}
+             </div>
+          </div>
+      );
+      const apiKeyField = (
+          <div className="form-group">
+             <input className={"form-input" + (this.state.error ? " is-error" : "")} type="text"
+                    placeholder="API Key"
+                    name="apiKey"
+                    value={this.state.env.apiKey} onChange={this.handleInputChange}/>
+             {this.state.error ?
+                 <p className="form-input-hint">Unable to verify credentials. Please check your domain and
+                    credentials.</p> : ""}
+          </div>
+      );
       return (
-          <form className="form-horizontal">
+          <form>
              <div className="form-group">
                 <input className="form-input" type="text" placeholder="Name (eg: production)" name="name"
                        value={this.state.env.name}
                        onChange={this.handleInputChange}/>
              </div>
-             <div className="form-group input-group">
-                <select className="form-select" value={this.state.env.protocol}>
+             <div className={"form-group input-group" + (this.state.error ? " has-error" : "")}>
+                <select name="protocol" className="form-select" value={this.state.env.protocol}
+                        onChange={this.handleInputChange}>
                    {Object.keys(PROTOCOL).map(key => <option value={PROTOCOL[key]}>{PROTOCOL[key]}</option>)}
                 </select>
                 <input className="form-input" type="text" placeholder="Domain (eg: xola.com)" name="domain"
                        value={this.state.env.domain} onChange={this.handleInputChange}/>
              </div>
              <div className="form-group">
-                <input className="form-input" type="text" placeholder="API Key" name="apiKey"
-                       value={this.state.env.apiKey} onChange={this.handleInputChange}/>
+                <label className="form-radio">
+                   <input type="radio" name="useApiKey" checked={!this.state.useApiKey} onChange={() => this.setState({useApiKey: false})}/>
+                   <i className="form-icon"></i> Email & Password
+                </label>
+                <label className="form-radio">
+                   <input type="radio" name="useApiKey" checked={this.state.useApiKey} onChange={() => this.setState({useApiKey: true})}/>
+                   <i className="form-icon"></i> API Key
+                </label>
              </div>
+             {this.state.useApiKey ? apiKeyField : emailIdFields}
              <div className="form-group">
                 <button className={"btn btn-block" + (this.state.loading ? " loading" : "")}
                         onClick={this.handleSubmit}>
@@ -87,12 +135,36 @@ class AddEnvironmentConfig extends Component {
       );
    }
 
+   testConnection(env) {
+      const headers = {};
+      if (env.apiKey) {
+         headers["X-API-KEY"] = env.apiKey;
+      } else {
+         headers["Authorization"] = "Basic " + btoa(`${env.email}:${env.password}`)
+      }
+
+      return $.ajax({
+         url: `${env.url}/api/users/me`,
+         headers
+      });
+   }
+
    handleSubmit = (e) => {
       e.preventDefault();
-      if (this.state.env.name && this.state.env.domain && this.state.env.apiKey) {
+      if (this.state.env.name && this.state.env.domain && (this.state.env.apiKey || this.state.env.email)) {
          this.setState({loading: true});
-         this.props.onAddConfig({name: this.state.env.name, domain: this.state.env.url, apiKey: this.state.env.apiKey});
-         this.setState({env: {name: '', domain: '', apiKey: ''}});
+         this.testConnection(this.state.env)
+             .then((resp) => {
+                this.props.onAddConfig({
+                   name: this.state.env.name,
+                   domain: this.state.env.url,
+                   apiKey: resp.apiKey
+                });
+                this.setState(this.getDefaultState());
+             })
+             .catch(() => {
+                this.setState({loading: false, error: true});
+             });
       }
    };
 
