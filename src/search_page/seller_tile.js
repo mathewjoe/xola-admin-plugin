e8c0210798ab2a943f45b90bb507c346fcc56a6f
Mathew Joe Thomas <disrupticons@gmail.com>
2018-03-07 10:50:55 +0530

    Add link to open a seller in admin app

--- src/search_page/seller_tile.js
+++ src/search_page/seller_tile.js
@@ -3,17 +3,21 @@ import React, {Component} from 'react';
 
 class SellerTile extends Component {
    render() {
+      const impersonateUrl = `${this.props.selectedEnv.baseUrl}/seller#seller=${this.props.seller.id}#apiKey=${this.props.selectedEnv.apiKey}`;
+      const adminAppUrl = `${this.props.selectedEnv.baseUrl}/admin#sellers/${this.props.seller.id}#apiKey=${this.props.selectedEnv.apiKey}`;
+      const imgUrl = `${this.props.selectedEnv.baseUrl}/api/users/${this.props.seller.id}/picture?size=small`;
       return (
-          <a href={`${this.props.selectedEnv.baseUrl}/seller#seller=${this.props.seller.id}#apiKey=${this.props.selectedEnv.apiKey}`} target="_blank">
-             <div className="tile tile-centered">
+          <a href={impersonateUrl} target="_blank">
+             <div className="seller-tile tile tile-centered">
                 <div className="tile-icon">
-                   <img src={`${this.props.selectedEnv.baseUrl}/api/users/${this.props.seller.id}/picture?size=small`}
-                        className="avatar avatar-sm"
-                        alt={this.props.seller.name}/>
+                   <img src={imgUrl} className="avatar avatar-sm" alt={this.props.seller.name}/>
                 </div>
                 <div className="tile-content">
                    <div className="tile-title">{this.props.seller.name}</div>
-                   <div className="title-subtitle">{this.props.seller.email}</div>
+                   <div className="title-subtitle">
+                      <div>{this.props.seller.email}</div>
+                      <div className="admin-app-link"><a href={adminAppUrl} target="_blank">Open Admin App</a></div>
+                   </div>
                 </div>
              </div>
           </a>
