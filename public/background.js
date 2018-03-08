68db20d1b82b3ac0233166bc522f6a2799996426
Mathew Joe Thomas <disrupticons@gmail.com>
2018-03-09 02:51:23 +0530

    Add reimpersonate to chrome's context menu

---
+++ public/background.js
@@ -0,0 +1,13 @@
+/* global chrome */
+chrome.runtime.onInstalled.addListener(details => {
+   chrome.contextMenus.create({
+      title: 'Re-impersonate',
+      id: 'reimpersonate'
+   });
+});
+
+chrome.contextMenus.onClicked.addListener(function(itemData, tab) {
+   if (itemData.menuItemId === "reimpersonate" && /xola[a-z.]*\/seller/.test(tab.url)) {
+      chrome.tabs.sendMessage(tab.id, {reimpersonate: true});
+   }
+});
