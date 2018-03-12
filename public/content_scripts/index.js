2453f2258815648d5cf4cd4e94c038b3bd8281bc
Mathew Joe Thomas <disrupticons@gmail.com>
2018-03-12 17:25:51 +0530

    Immediately execute content script

--- public/content_scripts/index.js
+++ public/content_scripts/index.js
@@ -1,14 +1,14 @@
-const s = document.createElement('script');
-s.src = chrome.extension.getURL('content_scripts/reimpersonate.js');
+(function() {
+   const s = document.createElement('script');
+   s.src = chrome.extension.getURL('content_scripts/reimpersonate.js');
 
-function __reimpersonate() {
-   (document.head || document.documentElement).appendChild(s);
-}
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-   if (request.reimpersonate) {
-      __reimpersonate();
+   function __reimpersonate() {
+      (document.head || document.documentElement).appendChild(s);
    }
-});
 
-
+   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      if (request.reimpersonate) {
+         __reimpersonate();
+      }
+   });
+})();
