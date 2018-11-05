7970f256bd234b31863f8d1210d32dfe91d011a6
Mathew Joe Thomas <disrupticons@gmail.com>
2018-11-05 21:04:07 +0530

    Show 'Generate bookings' context menu item only for development and sandbox environments

--- public/background.js
+++ public/background.js
@@ -1,31 +1,30 @@
 /* global chrome */
 chrome.runtime.onInstalled.addListener(details => {
-  chrome.contextMenus.create({
-    title: 'Re-impersonate',
-    id: 'reimpersonate'
-  })
+    chrome.contextMenus.create({
+        title: 'Re-impersonate',
+        id: 'reimpersonate'
+    })
 
-  chrome.contextMenus.create({
-    title: 'Generate Demo Bookings',
-    id: 'generateDemoBooking'
-  })
+    const bookingGeneratorWhitelist = [
+        'http://xola.local/seller',
+        'https://sandbox1.xola.com/seller',
+        'https://staging1.xola.com/seller',
+    ];
+
+    chrome.contextMenus.create({
+        title: 'Generate Demo Bookings',
+        id: 'generateDemoBooking',
+        documentUrlPatterns: bookingGeneratorWhitelist
+    })
 })
 
 chrome.contextMenus.onClicked.addListener(function (itemData, tab) {
-  if (/xola[a-z.]*\/seller/.test(tab.url)) {
-    if (itemData.menuItemId === 'reimpersonate') {
-      chrome.tabs.sendMessage(tab.id, {action: 'reimpersonate'})
-    } else if (itemData.menuItemId === 'generateDemoBooking') {
-      const isOnC02 = /^https:\/\/c02.xola.com/.test(tab.url)
-      const isOnStable = /^https:\/\/stable.xola.com/.test(tab.url)
-      const isOnSilent = /^https:\/\/silent.xola.com/.test(tab.url)
-      const isOnDirect = /^https:\/\/direct.xola.com/.test(tab.url)
-      if (isOnC02 || isOnStable || isOnSilent || isOnDirect) {
-        alert('Do not create demo bookings on Prod')
-      } else {
-        chrome.tabs.sendMessage(tab.id, {action: 'generateDemoBooking'})
-      }
+    if (/xola[a-z.]*\/seller/.test(tab.url)) {
+        if (itemData.menuItemId === 'reimpersonate') {
+            chrome.tabs.sendMessage(tab.id, {action: 'reimpersonate'})
+        } else if (itemData.menuItemId === 'generateDemoBooking') {
+            chrome.tabs.sendMessage(tab.id, {action: 'generateDemoBooking'})
+        }
     }
-  }
 })
 
