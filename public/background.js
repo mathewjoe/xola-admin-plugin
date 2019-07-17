b10f9a505b657e4463552aede8d99103b4dd9553
Derick Mathew <derick@xola.com>
2019-07-17 12:18:52 +0530

    remove the booking generator + its dependencies + changes to handle inactivity on ext startup

--- public/background.js
+++ public/background.js
@@ -3,28 +3,14 @@ chrome.runtime.onInstalled.addListener(details => {
     chrome.contextMenus.create({
         title: 'Re-impersonate',
         id: 'reimpersonate'
-    })
-
-    const bookingGeneratorWhitelist = [
-        'http://xola.local/seller',
-        'https://sandbox1.xola.com/seller',
-        'https://staging1.xola.com/seller',
-    ];
-
-    chrome.contextMenus.create({
-        title: 'Generate Bookings',
-        id: 'generateBookings',
-        documentUrlPatterns: bookingGeneratorWhitelist
-    })
-})
+    });
+});
 
 chrome.contextMenus.onClicked.addListener(function (itemData, tab) {
     if (/xola[a-z.]*\/seller/.test(tab.url)) {
         if (itemData.menuItemId === 'reimpersonate') {
             chrome.tabs.sendMessage(tab.id, {action: 'reimpersonate'})
-        } else if (itemData.menuItemId === 'generateBookings') {
-            chrome.tabs.sendMessage(tab.id, {action: 'generateBookings'})
         }
     }
-})
+});
 
--- public/content_scripts/index.js
+++ public/content_scripts/index.js
@@ -1,8 +1,6 @@
 (function () {
   const reimporsonateEl = document.createElement('script')
   reimporsonateEl.src = chrome.extension.getURL('content_scripts/reimpersonate.js')
-  const bookingGeneratorInjectorEl = document.createElement('script')
-  bookingGeneratorInjectorEl.src = chrome.extension.getURL('content_scripts/booking_generator_injector.js')
 
   function __reimpersonate () {
     (document.head || document.documentElement).appendChild(reimporsonateEl)
@@ -12,24 +10,6 @@
     switch (request.action) {
       case 'reimpersonate':
         __reimpersonate()
-        break
-      case 'generateBookings':
-        (document.head || document.documentElement).appendChild(bookingGeneratorInjectorEl)
-
-        let message = "How many bookings do you want to generate? (Max: 100) \n\nNote: The actual number of bookings generated could be lower than this number because of lack of availability, API errors etc..\n"
-        let requiredCount = prompt(message, '50');
-        if (requiredCount) {
-          setTimeout(function () {
-            const messengerDivEl = document.getElementById('demo-booking-generator')
-            chrome.extension.sendMessage({
-              action: 'backgroundBookingGenerator',
-              baseUrl: messengerDivEl.getAttribute('baseUrl'),
-              apiKey: messengerDivEl.getAttribute('apiKey'),
-              sellerId: messengerDivEl.getAttribute('sellerId'),
-              requiredCount: requiredCount
-            })
-          }, 1000)
-        }
         break
     }
   })
