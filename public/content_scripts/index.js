6f30e7882557648a6e825a7c573007c32e071c45
Derick Mathew <derick@xola.com>
2019-08-05 12:36:05 +0530

    Fixed Reimpersonate, was previously passing wrong payload in sendMessage

--- public/content_scripts/index.js
+++ public/content_scripts/index.js
@@ -1,14 +1,14 @@
 (function () {
-  const reimporsonateEl = document.createElement('script')
-  reimporsonateEl.src = chrome.extension.getURL('content_scripts/reimpersonate.js')
+  const reimporsonateEl = document.createElement('script');
+  reimporsonateEl.src = chrome.extension.getURL('content_scripts/reimpersonate.js');
 
-  function __reimpersonate () {
-    (document.head || document.documentElement).appendChild(reimporsonateEl)
+  function __reimpersonate() {
+    (document.head || document.documentElement).appendChild(reimporsonateEl);
   }
 
   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.reimpersonate) {
-        __reimpersonate()
+        __reimpersonate();
     }
-  })
+  });
 })();
